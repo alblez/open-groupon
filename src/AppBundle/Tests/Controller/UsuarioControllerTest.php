3c487e2fccb4ca3dfeb314ab6afae7d561397926
Alberto Gonzalez Rouille <spin.jag@gmail.com>
2012-07-17 23:56:19 -0500

    test: update usuario tests

--- src/AppBundle/Tests/Controller/UsuarioControllerTest.php
+++ src/AppBundle/Tests/Controller/UsuarioControllerTest.php
@@ -3,8 +3,8 @@
 /*
  * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
  *
- * Este archivo pertenece a la aplicación de prueba Cupon.
- * El código fuente de la aplicación incluye un archivo llamado LICENSE
+ * Este file pertenece a la application de prueba Cupon.
+ * El code fuente de la application incluye un file llamado LICENSE
  * con toda la información sobre el copyright y la licencia.
  */
 
@@ -13,67 +13,67 @@ namespace AppBundle\Tests\Controller;
 use AppBundle\Test\BaseTestCase;
 
 /**
- * Test funcional para comprobar que funciona bien el registro de usuarios
- * en el frontend, además del perfil y el proceso de baja del usuario.
+ * Test funcional para comprobar que funciona bien el record de usuarios
+ * en el frontend, además del perfil y el proceso de baja del user.
  */
 class UsuarioControllerTest extends BaseTestCase
 {
     /**
      * @dataProvider usuarios
      */
-    public function testRegistroPerfilBaja($usuario)
+    public function testRegistroPerfilBaja($user)
     {
         $client = static::createClient();
         $client->followRedirects(true);
 
         $crawler = $client->request('GET', '/');
 
-        // Registrarse como nuevo usuario
+        // Registrarse como nuevo user
         $enlaceRegistro = $crawler->selectLink('Regístrate ya')->link();
         $crawler = $client->click($enlaceRegistro);
 
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Regístrate gratis como usuario")')->count(),
-            'Después de pulsar el botón Regístrate de la portada, se carga la página con el formulario de registro'
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Regístrate gratis como user")')->count(),
+            'Después de pulsar el botón Regístrate de la portada, se carga la page con el form de record'
         );
 
-        $formulario = $crawler->selectButton('Registrarme')->form($usuario);
-        $crawler = $client->submit($formulario);
+        $form = $crawler->selectButton('Registrarme')->form($user);
+        $crawler = $client->submit($form);
 
         $this->assertTrue($client->getResponse()->isSuccessful());
 
         // Comprobar que el cliente ahora dispone de una cookie de sesión
         $this->assertRegExp('/(\d|[a-z])+/', $client->getCookieJar()->get('MOCKSESSID', '/', 'localhost')->getValue(),
-            'La aplicación ha enviado una cookie de sesión'
+            'La application ha enviado una cookie de sesión'
         );
 
-        // Acceder al perfil del usuario recién creado
+        // Acceder al perfil del user recién creado
         $perfil = $crawler->filter('aside section#login')->selectLink('Ver mi perfil')->link();
         $crawler = $client->click($perfil);
 
         $this->assertEquals(
-            $usuario['frontend_usuario[email]'],
+            $user['frontend_usuario[email]'],
             $crawler->filter('form input[name="frontend_usuario[email]"]')->attr('value'),
-            'El usuario se ha registrado correctamente y sus datos se han guardado en la base de datos'
+            'El user se ha registrado correctamente y sus datos se han guardado en la base de datos'
         );
     }
 
     /**
-     * Método que provee de usuarios de prueba a los tests de esta clase.
+     * method que provee de usuarios de prueba a los tests de esta clase.
      */
     public function usuarios()
     {
         return array(
             array(
                 array(
-                    'frontend_usuario[nombre]' => 'Anónimo',
+                    'frontend_usuario[name]' => 'Anónimo',
                     'frontend_usuario[apellidos]' => 'Apellido1 Apellido2',
                     'frontend_usuario[email]' => 'anonimo'.uniqid().'@localhost.localdomain',
-                    'frontend_usuario[password][first]' => 'anonimo1234',
-                    'frontend_usuario[password][second]' => 'anonimo1234',
-                    'frontend_usuario[direccion]' => 'Mi calle, Mi ciudad, 01001',
+                    'frontend_usuario[passwordEnClaro][first]' => 'anonimo1234',
+                    'frontend_usuario[passwordEnClaro][second]' => 'anonimo1234',
+                    'frontend_usuario[direccion]' => 'Mi calle, Mi city, 01001',
                     'frontend_usuario[dni]' => '11111111H',
                     'frontend_usuario[numeroTarjeta]' => '123456789012345',
-                    'frontend_usuario[ciudad]' => '1',
+                    'frontend_usuario[city]' => '1',
                     'frontend_usuario[permiteEmail]' => '1',
                 ),
             ),
