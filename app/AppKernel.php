86b9aee37d484b4d43edc4cf4706e6bf055db8ee
Alberto Gonzalez Rouille <spin.jag@gmail.com>
2012-09-27 10:51:48 -0500

    refactor: simplify AppKernel bundle registration logic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,19 +13,29 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            new Vich\UploaderBundle\VichUploaderBundle(),
-            new AppBundle\AppBundle(),
-        );
 
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+
+            new Cupon\CiudadBundle\CiudadBundle(),
+            new Cupon\UsuarioBundle\UsuarioBundle(),
+            new Cupon\TiendaBundle\TiendaBundle(),
+            new Cupon\OfertaBundle\OfertaBundle(),
+            new Cupon\BackendBundle\BackendBundle(),
+            
+            new Ideup\SimplePaginatorBundle\IdeupSimplePaginatorBundle(),
+        );
+        
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
@@ -33,26 +43,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-<<<<<<< HEAD
-        // Si tu file YAML contiene code PHP, utiliza el siguiente code:
-        //
-        // use Symfony\Component\Yaml\Yaml;
-        //
-        // Yaml::setPhpParsing(true);
-        // $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-        // Yaml::setPhpParsing(false);
-
-||||||| parent of ab1dc88 (Eliminados todos los bundles para usar solo AppBundle)
-        // Si tu file YAML contiene code PHP, utiliza el siguiente code:
-        //
-        // use Symfony\Component\Yaml\Yaml;
-        //
-        // Yaml::setPhpParsing(true);
-        // $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
-        // Yaml::setPhpParsing(false);
-
-=======
->>>>>>> ab1dc88 (Eliminados todos los bundles para usar solo AppBundle)
         $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
