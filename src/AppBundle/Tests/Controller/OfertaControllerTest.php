918ecf23eedb577cea660d282648a7c063885b9c
Alberto Gonzalez Rouille <spin.jag@gmail.com>
2016-03-23 19:12:11 -0500

    test: fix oferta tests

--- src/AppBundle/Tests/Controller/OfertaControllerTest.php
+++ src/AppBundle/Tests/Controller/OfertaControllerTest.php
@@ -3,61 +3,53 @@
 /*
  * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
  *
- * Este file pertenece a la application de prueba Cupon.
- * El code fuente de la application incluye un file llamado LICENSE
+ * Este archivo pertenece a la aplicación de prueba Cupon.
+ * El código fuente de la aplicación incluye un archivo llamado LICENSE
  * con toda la información sobre el copyright y la licencia.
  */
 
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Test funcional de la portada del sitio y de la acción de comprar una offer
- * por parte de un user anónimo.
+ * Test funcional de la portada del sitio y de la acción de comprar una oferta
+ * por parte de un usuario anónimo.
  *
- * También asegura el rendimiento de la application obligando a que la portada
+ * También asegura el rendimiento de la aplicación obligando a que la portada
  * requiera menos de cuatro consultas a la base de datos y se genere en menos
  * de medio segundo.
  */
 class OfertaControllerTest extends WebTestCase
 {
-    /** @test */
-    public function laPortadaSeGeneraCorrectamente()
+    public function testLaPortadaSeGeneraCorrectamente()
     {
         $client = static::createClient();
-
-        //SUT
         $client->request('GET', '/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(),
+        $this->assertEquals(Response::HTTP_OK, $client->getResponse()->getStatusCode(),
             'La portada se genera correctamente.'
         );
     }
 
-    /** @test */
-    public function laPortadaSoloMuestraUnaOfertaActiva()
+    public function testLaPortadaSoloMuestraUnaOfertaActiva()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        //SUT
-        $ofertasActivas = $crawler->filter(
-            'article.offer section.descripcion a:contains("Comprar")'
-        )->count();
+        $ofertasActivas = $crawler->filter('article.oferta section.descripcion a:contains("Comprar")');
 
-        $this->assertEquals(1, $ofertasActivas,
-            'La portada muestra una única offer activa que se puede comprar'
+        $this->assertCount(1, $ofertasActivas,
+            'La portada muestra una única oferta activa que se puede comprar'
         );
     }
 
-    /** @test */
-    public function losUsuariosPuedenRegistrarseDesdeLaPortada()
+    public function testLosUsuariosPuedenRegistrarseDesdeLaPortada()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        //SUT
         $numeroEnlacesRegistrarse = $crawler->filter('html:contains("Regístrate")')->count();
 
         $this->assertGreaterThan(0, $numeroEnlacesRegistrarse,
@@ -65,62 +57,55 @@ class OfertaControllerTest extends WebTestCase
         );
     }
 
-    /** @test */
-    public function losUsuariosAnonimosVenLaCiudadPorDefecto()
+    public function testLosUsuariosAnonimosVenLaCiudadPorDefecto()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        //SUT
         $ciudadPorDefecto = $client->getContainer()->getParameter('cupon.ciudad_por_defecto');
         $ciudadPortada = $crawler->filter('header nav select option[selected="selected"]')->attr('value');
 
         $this->assertEquals($ciudadPorDefecto, $ciudadPortada,
-            'La city seleccionada en la portada de un user anónimo es la city por defecto'
+            'La ciudad seleccionada en la portada de un usuario anónimo es la ciudad por defecto'
         );
     }
 
-    /** @test */
-    public function losUsuariosAnonimosNoPuedenComprar()
+    public function testLosUsuariosAnonimosNoPuedenComprar()
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
 
-        //SUT
         $comprar = $crawler->selectLink('Comprar')->link();
         $client->click($comprar);
 
         $this->assertTrue($client->getResponse()->isRedirect(),
-            'Cuando un user anónimo intenta comprar, se le redirige al form de login'
+            'Cuando un usuario anónimo intenta comprar, se le redirige al formulario de login'
         );
     }
 
-    /** @test */
-    public function losUsuariosAnonimosDebenLoguearseParaPoderComprar()
+    public function testLosUsuariosAnonimosDebenLoguearseParaPoderComprar()
     {
-        $pathLogin = '/.*\/user\/login_check/';
+        $pathLogin = '/.*\/usuario\/login_check/';
         $client = static::createClient();
         $client->followRedirects(true);
 
         $crawler = $client->request('GET', '/');
 
-        //SUT
         $comprar = $crawler->selectLink('Comprar')->link();
         $crawler = $client->click($comprar);
 
         $this->assertRegExp($pathLogin, $crawler->filter('article form')->attr('action'),
-            'Después de pulsar el botón de comprar, el user anónimo ve el form de login'
+            'Después de pulsar el botón de comprar, el usuario anónimo ve el formulario de login'
         );
     }
 
-    /** @test */
-    public function laPortadaRequierePocasConsultasDeBaseDeDatos()
+    public function testLaPortadaRequierePocasConsultasDeBaseDeDatos()
     {
         $client = static::createClient();
         $client->enableProfiler();
 
         $client->request('GET', '/');
-        //SUT
+
         if ($profiler = $client->getProfile()) {
             $this->assertLessThan(4, count($profiler->getCollector('db')->getQueries()),
                 'La portada requiere menos de 4 consultas a la base de datos'
@@ -128,8 +113,7 @@ class OfertaControllerTest extends WebTestCase
         }
     }
 
-    /** @test */
-    public function laPortadaSeGeneraMuyRapido()
+    public function testLaPortadaSeGeneraMuyRapido()
     {
         $client = static::createClient();
         $client->enableProfiler();
