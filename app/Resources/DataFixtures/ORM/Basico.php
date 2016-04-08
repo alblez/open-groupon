94033543875a5121538a92d3f01a4083241dab3f
Alberto Gonzalez Rouille <spin.jag@gmail.com>
2016-04-07 20:50:50 -0500

    test: update fixture data with seed values for testing

--- app/Resources/DataFixtures/ORM/Basico.php
+++ app/Resources/DataFixtures/ORM/Basico.php
@@ -3,8 +3,8 @@
 /*
  * (c) Javier Eguiluz <javier.eguiluz@gmail.com>
  *
- * Este archivo pertenece a la aplicación de prueba Cupon.
- * El código fuente de la aplicación incluye un archivo llamado LICENSE
+ * Este file pertenece a la application de prueba Cupon.
+ * El code fuente de la application incluye un file llamado LICENSE
  * con toda la información sobre el copyright y la licencia.
  */
 
@@ -12,35 +12,37 @@ use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Cupon\CiudadBundle\Entity\Ciudad;
-use Cupon\OfertaBundle\Entity\Oferta;
-use Cupon\TiendaBundle\Entity\Tienda;
-use Cupon\UsuarioBundle\Entity\Usuario;
-use Cupon\OfertaBundle\Entity\Venta;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use AppBundle\Entity\city;
+use AppBundle\Entity\offer;
+use AppBundle\Entity\store;
+use AppBundle\Entity\user;
+use AppBundle\Entity\sale;
 
 /**
- * Versión simplificada de los fixtures completos de la aplicación.
- * Se ha eliminado todo el código que hace uso de la ACL y del componente
- * de seguridad.
+ * Versión simplificada de los fixtures completos de la application.
+ * Se ha eliminado todo el code que hace uso de la ACL y del componente
+ * de security.
  *
- * Este es el archivo que debes utilizar si estás creando la aplicación a mano
- * y todavía no has llegado al capítulo de la seguridad. Carga estos fixtures
+ * Este es el file que debes utilizar si estás creando la application a mano
+ * y todavía no has llegado al capítulo de la security. Carga estos fixtures
  * básicos ejecutando el siguiente comando:
  *
  * $ php app/console doctrine:fixtures:load --fixtures=app/Resources
  * 
- * Al utilizar este archivo de datos simplificado, la configuración de seguridad
- * de la aplicación debe indicar que los usuarios de tipo `Usuario` guardan la
- * contraseña en claro, sin codificar.
+ * Al utilizar este file de datos simplificado, la configuration de security
+ * de la application debe indicar que los usuarios de type `user` guardan la
+ * password en claro, sin codificar.
  * 
- * Asegúrate de que en el archivo `security.yml` tengas la siguiente configuración:
+ * Asegúrate de que en el file `security.yml` tengas la siguiente configuration:
  *   security:
  *     # ...
  *     encoders:
- *       Cupon\UsuarioBundle\Entity\Usuario: plaintext
+ *       Cupon\UsuarioBundle\Entity\user: plaintext
  */
 class Basico implements FixtureInterface, ContainerAwareInterface
 {
+    /** @var ContainerInterface */
     private $container;
 
     public function setContainer(ContainerInterface $container = null)
@@ -51,169 +53,167 @@ class Basico implements FixtureInterface, ContainerAwareInterface
     public function load(ObjectManager $manager)
     {
         // Crear 5 ciudades de prueba
-        foreach (array('Madrid', 'Barcelona', 'Castellón', 'Vigo', 'Vitoria-Gasteiz') as $nombre) {
-            $ciudad = new Ciudad();
-            $ciudad->setNombre($nombre);
+        foreach (array('Madrid', 'Barcelona', 'Castellón', 'Vigo', 'Vitoria-Gasteiz') as $name) {
+            $city = new city();
+            $city->setNombre($name);
 
-            $manager->persist($ciudad);
+            $manager->persist($city);
         }
 
         $manager->flush();
 
-        // Crear 10 tiendas en cada ciudad
-        $ciudades = $manager->getRepository('CiudadBundle:Ciudad')->findAll();
+        // Crear 10 tiendas en cada city
+        $ciudades = $manager->getRepository('AppBundle:city')->findAll();
         $numTienda = 0;
-        foreach ($ciudades as $ciudad) {
+        foreach ($ciudades as $city) {
             for ($i = 1; $i <= 10; ++$i) {
                 ++$numTienda;
 
-                $tienda = new Tienda();
-                $tienda->setNombre('Tienda #'.$numTienda);
-                $tienda->setLogin('tienda'.$numTienda);
-                $tienda->setPassword('password'.$numTienda);
-                $tienda->setSalt(md5(time()));
-                $tienda->setDescripcion(
+                $store = new store();
+                $store->setNombre('store #'.$numTienda);
+                $store->setLogin('store'.$numTienda);
+                $store->setPassword('password'.$numTienda);
+                $store->setDescripcion(
                     'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'
                     .'sed do eiusmod tempor incididunt ut labore et dolore magna'
                     .'aliqua. Ut enim ad minim veniam, quis nostrud exercitation'
                     .'ullamco laboris nisi ut aliquip ex ea commodo consequat.'
                 );
-                $tienda->setDireccion("Calle Lorem Ipsum, $i\n".$ciudad->getNombre());
-                $tienda->setCiudad($ciudad);
+                $store->setDireccion("Calle Lorem Ipsum, $i\n".$city->getNombre());
+                $store->setCiudad($city);
 
-                $manager->persist($tienda);
+                $manager->persist($store);
             }
         }
         $manager->flush();
 
-        // Crear 50 ofertas en cada ciudad
-        $ciudades = $manager->getRepository('CiudadBundle:Ciudad')->findAll();
+        // Crear 50 ofertas en cada city
+        $ciudades = $manager->getRepository('AppBundle:city')->findAll();
         $numOferta = 0;
-        foreach ($ciudades as $ciudad) {
-            $tiendas = $manager->getRepository('TiendaBundle:Tienda')->findByCiudad(
-                $ciudad->getId()
+        foreach ($ciudades as $city) {
+            $tiendas = $manager->getRepository('AppBundle:store')->findByCiudad(
+                $city->getId()
             );
 
             for ($i = 1; $i <= 50; ++$i) {
                 ++$numOferta;
 
-                $oferta = new Oferta();
+                $offer = new offer();
 
-                $oferta->setNombre('Oferta #'.$numOferta.' lorem ipsum dolor sit amet');
-                $oferta->setDescripcion(
+                $offer->setNombre('offer #'.$numOferta.' lorem ipsum dolor sit amet');
+                $offer->setDescripcion(
                     "Lorem ipsum dolor sit amet, consectetur adipisicing.\n"
                     ."Elit, sed do eiusmod tempor incididunt.\n"
                     ."Ut labore et dolore magna aliqua.\n"
                     .'Nostrud exercitation ullamco laboris nisi ut'
                 );
-                $oferta->setCondiciones('Labore et dolore magna aliqua. Ut enim ad minim veniam.');
-                $oferta->setFoto('foto'.rand(1, 20).'.jpg');
-                $oferta->setPrecio(number_format(rand(100, 10000) / 100, 2));
-                $oferta->setDescuento($oferta->getPrecio() * (rand(10, 70) / 100));
+                $offer->setCondiciones('Labore et dolore magna aliqua. Ut enim ad minim veniam.');
+                $offer->setRutaFoto('photo'.rand(1, 20).'.jpg');
+                $offer->setPrecio(number_format(rand(100, 10000) / 100, 2));
+                $offer->setDescuento($offer->getPrecio() * (rand(10, 70) / 100));
 
                 // Se publican 9 ofertas en el pasado, 1 en el presente y 40 en el futuro
                 if (1 == $i) {
-                    $fecha = 'today';
-                    $oferta->setRevisada(true);
+                    $date = 'today';
+                    $offer->setRevisada(true);
                 } elseif ($i < 10) {
-                    $fecha = 'now - '.($i - 1).' days';
+                    $date = 'now - '.($i - 1).' days';
                     // el 80% de las ofertas pasadas se marcan como revisadas
-                    $oferta->setRevisada((rand(1, 1000) % 10) < 8);
+                    $offer->setRevisada((rand(1, 1000) % 10) < 8);
                 } else {
-                    $fecha = 'now + '.($i - 10 + 1).' days';
-                    $oferta->setRevisada(true);
+                    $date = 'now + '.($i - 10 + 1).' days';
+                    $offer->setRevisada(true);
                 }
 
-                $fechaPublicacion = new \DateTime($fecha);
+                $fechaPublicacion = new \DateTime($date);
                 $fechaPublicacion->setTime(23, 59, 59);
 
                 $fechaExpiracion = clone $fechaPublicacion;
                 $fechaExpiracion->add(\DateInterval::createFromDateString('24 hours'));
 
-                $oferta->setFechaPublicacion($fechaPublicacion);
-                $oferta->setFechaExpiracion($fechaExpiracion);
+                $offer->setFechaPublicacion($fechaPublicacion);
+                $offer->setFechaExpiracion($fechaExpiracion);
 
-                $oferta->setCompras(0);
-                $oferta->setUmbral(rand(25, 100));
+                $offer->setCompras(0);
+                $offer->setUmbral(rand(25, 100));
 
-                $oferta->setCiudad($ciudad);
+                $offer->setCiudad($city);
 
-                // Seleccionar aleatoriamente una tienda que pertenezca a la ciudad
-                $oferta->setTienda($tiendas[array_rand($tiendas)]);
+                // Seleccionar aleatoriamente una store que pertenezca a la city
+                $offer->setTienda($tiendas[array_rand($tiendas)]);
 
-                $manager->persist($oferta);
+                $manager->persist($offer);
             }
         }
         $manager->flush();
 
-        // Crear 100 usuarios en cada ciudad
+        // Crear 100 usuarios en cada city
         $numUsuario = 0;
-        foreach ($ciudades as $ciudad) {
+        foreach ($ciudades as $city) {
             for ($i = 1; $i <= 100; ++$i) {
                 ++$numUsuario;
 
-                $usuario = new Usuario();
+                $user = new user();
 
-                $usuario->setNombre('Usuario #'.$numUsuario);
-                $usuario->setApellidos('Apellido1 Apellido2');
-                $usuario->setEmail('usuario'.$numUsuario.'@localhost');
-                $usuario->setSalt('');
-                $usuario->setPassword('password'.$numUsuario);
-                $usuario->setDireccion("Calle Ipsum Lorem, 2\n".$ciudad->getNombre());
+                $user->setNombre('user #'.$numUsuario);
+                $user->setApellidos('Apellido1 Apellido2');
+                $user->setEmail('user'.$numUsuario.'@localhost');
+                $user->setPassword('password'.$numUsuario);
+                $user->setDireccion("Calle Ipsum Lorem, 2\n".$city->getNombre());
                 // El 60% de los usuarios permite email
-                $usuario->setPermiteEmail((rand(1, 1000) % 10) < 6);
-                $usuario->setFechaAlta(new \DateTime('now - '.rand(1, 150).' days'));
-                $usuario->setFechaNacimiento(new \DateTime('now - '.rand(7000, 20000).' days'));
+                $user->setPermiteEmail((rand(1, 1000) % 10) < 6);
+                $user->setFechaAlta(new \DateTime('now - '.rand(1, 150).' days'));
+                $user->setFechaNacimiento(new \DateTime('now - '.rand(7000, 20000).' days'));
 
                 $dni = substr(rand(), 0, 8);
-                $usuario->setDni($dni.substr(
+                $user->setDni($dni.substr(
                     'TRWAGMYFPDXBNJZSQVHLCKE',
                     strtr($dni, 'XYZ', '012') % 23, 1)
                 );
 
-                $usuario->setNumeroTarjeta('1234567890123456');
-                $usuario->setCiudad($ciudad);
+                $user->setNumeroTarjeta('1234567890123456');
+                $user->setCiudad($city);
 
-                $manager->persist($usuario);
+                $manager->persist($user);
             }
         }
         $manager->flush();
 
         // Crear 500 ventas aleatorias
-        $ofertas = $manager->getRepository('OfertaBundle:Oferta')->findAll();
-        $usuarios = $manager->getRepository('UsuarioBundle:Usuario')->findAll();
+        $ofertas = $manager->getRepository('AppBundle:offer')->findAll();
+        $usuarios = $manager->getRepository('AppBundle:user')->findAll();
 
-        foreach ($usuarios as $usuario) {
+        foreach ($usuarios as $user) {
             $compras = rand(0, 10);
             $comprado = array();
 
             for ($i = 0; $i < $compras; ++$i) {
-                $venta = new Venta();
+                $sale = new sale();
 
-                // Sólo se añade una venta:
-                //   - si este mismo usuario no ha comprado antes la misma oferta
-                //   - si la oferta seleccionada ha sido revisada
-                //   - si la fecha de publicación de la oferta es posterior a ahora mismo
-                $oferta = $ofertas[array_rand($ofertas)];
-                while (in_array($oferta->getId(), $comprado)
-                       || $oferta->getRevisada() == false
-                       || $oferta->getFechaPublicacion() > new \DateTime('now')) {
-                    $oferta = $ofertas[array_rand($ofertas)];
+                // Sólo se añade una sale:
+                //   - si este mismo user no ha comprado antes la misma offer
+                //   - si la offer seleccionada ha sido revisada
+                //   - si la date de publicación de la offer es posterior a ahora mismo
+                $offer = $ofertas[array_rand($ofertas)];
+                while (in_array($offer->getId(), $comprado)
+                       || $offer->getRevisada() == false
+                       || $offer->getFechaPublicacion() > new \DateTime('now')) {
+                    $offer = $ofertas[array_rand($ofertas)];
                 }
-                $comprado[] = $oferta->getId();
+                $comprado[] = $offer->getId();
 
-                $venta->setOferta($oferta);
-                $venta->setUsuario($usuario);
+                $sale->setOferta($offer);
+                $sale->setUsuario($user);
 
-                $publicacion = clone $oferta->getFechaPublicacion();
-                $venta->setFecha(
+                $publicacion = clone $offer->getFechaPublicacion();
+                $sale->setFecha(
                     $publicacion->add(\DateInterval::createFromDateString(rand(10, 10000).' seconds'))
                 );
 
-                $manager->persist($venta);
+                $manager->persist($sale);
 
-                $oferta->setCompras($oferta->getCompras() + 1);
-                $manager->persist($oferta);
+                $offer->setCompras($offer->getCompras() + 1);
+                $manager->persist($offer);
             }
 
             unset($comprado);
